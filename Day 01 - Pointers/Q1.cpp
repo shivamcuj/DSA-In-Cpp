569f9817bd94917b440f7227f3c513753a4cf96b
Shivam Kumar <shivamjnv2305@gmail.com>
2026-03-30 21:22:45 +0530

    DAY 01 of DSA in Cpp

---
+++ Day 01 - Pointers/Q1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x=5,y=10;
+    int *ptr1 = &x, *ptr2 = &y;
+    ptr2 = ptr1;
+    cout << ptr2 << "\n";
+    cout << ptr1 << "\n";
+    return 0;
+}
---
+++ Day 01 - Pointers/Q2.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    float* a,b;
+    cout << a << "\n";
+    cout << b << "\n";
+    return 0;
+}
