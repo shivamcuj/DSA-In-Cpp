1371b22cf6404db874cfe5348a33947bdbb09149
Shivam Kumar <shivamjnv2305@gmail.com>
2026-09-15 21:20:05 +0530

    Create q1.cpp

---
+++ Basic Practice/Day 01 - Operator & Data Types/q1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Enter Side Length of A Square(Integer):\n";
+    int side;
+    cin>>side;
+    int area = side*side;
+    cout<<"Area of Square is "<<area;
+
+    return 0;
+}
